40f5996c48f9abf8a401079d75579900fce0b5a3
Alisa Ursu <ursualisa2004@gmail.com>
2025-05-08 13:43:27 +0300

    Testing app

---
+++ models/model_v2_2017.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class Model_v2_2017(nn.Module):
+    def __init__(self, input_dim=2351):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(2048, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.net(x)
